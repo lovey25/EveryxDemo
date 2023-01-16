5be0869bcf77a9d25097d07a62ca461580cf6442
lovey25 <kwonkyo@gmail.com>
2023-01-16 14:34:39 +0900

    Read text from file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import styled from "styled-components";
+import { useState } from "react";
 
-const Thing = styled.div`
-  color: blue;
-  &:hover {
-    color: red; // <Thing>요소 중 마우스가 올라간 모든 요소의 글씨는 붉은색
-  }
-  & ~ & {
-    background: tomato; // <Thing>의 형제 요소중 모든 <Thing>의 배경은 토마토색
-  }
-  & + & {
-    background: lime; // <Thing> 바로 다음에 오는 <Thing>의 배경은 라임색
-  }
-  &.something {
-    background: orange; // <Thing>요소 중 클래스명이 ".something"이면 배경을 오랜지색
-  }
-  .something-else & {
-    border: 1px solid; // 클래스명이 ".something-else"인 모든 요소중 자식 요소에 <Thing>이 있으면 테두리 그리기
-  }
-`;
+function App() {
+  const [data, setData] = useState([]);
 
-export default function App() {
+  const readFileData = async (e) => {
+    const reader = new FileReader();
+
+    try {
+      const file = e.target.files[0];
+
+      // reader.readAsArrayBuffer(file);
+      reader.onload = (result) => {
+        console.log("0");
+        // result = new Int8Array(result);
+        console.log("1", result.target);
+        setData(result.target.result);
+      };
+
+      reader.readAsText(file);
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
   return (
-    <div className="App">
-      <Thing>Hello world!!!</Thing>
-      <Thing>How ya doing?</Thing>
-      <Thing className="something">The sun is shining...</Thing>
-      <div>Pretty nice day today.</div>
-      <Thing>Don't you think?</Thing>
-      <div className="something-else">
-        <Thing>Splendid.</Thing>
-      </div>
-    </div>
+    <>
+      <input
+        type="file"
+        // accept="image/png, image/jpeg"
+        onChange={readFileData}
+      />
+      <br />
+      <br />
+      <div>{data}</div>
+    </>
   );
 }
+
+export default App;
