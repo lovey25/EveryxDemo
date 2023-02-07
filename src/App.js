f7176465487fb3e34484075a092889f29c776db5
kwonkyo <kwonkyo@gmail.com>
2023-02-07 14:49:51 +0900

    Save binary and string data to local disk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 export default function App() {
-  const onClick = () => {
-    var tempLink = document.createElement("a");
+  async function saveFile() {
     let textArea = document.querySelector("textarea");
-    var taBlob = new Blob([textArea.value], { type: "text/plain" });
-    tempLink.setAttribute("href", URL.createObjectURL(taBlob));
-    tempLink.setAttribute("download", `everyx.txt`);
-    tempLink.click();
-    URL.revokeObjectURL(tempLink.href);
-  };
+    var taBlob = new Blob([textArea.value], {
+      type: "application/octet-stream",
+    });
+    const fileHandle = await window.showSaveFilePicker();
+    const fileStream = await fileHandle.createWritable();
+
+    await fileStream.write(taBlob);
+    await fileStream.close();
+  }
+
+  async function saveBinFile() {
+    let textArea = document.querySelector("textarea");
+    const data = textArea.value.split(",");
+    const test = new Uint16Array(data);
+    const fileHandle = await window.showSaveFilePicker();
+    const fileStream = await fileHandle.createWritable();
+    var taBlob = new Blob([test], {
+      type: "application/octet-stream",
+    });
+    await fileStream.write(taBlob);
+    await fileStream.close();
+  }
 
   return (
     <>
       <div>
-        <textarea></textarea>
+        <textarea defaultValue={"500, 700, 800, 250"}></textarea>
       </div>
-      <button className="save-file" onClick={onClick}>
+      <button className="save-file" onClick={saveFile}>
         Save File
       </button>
-      <a className="simple" style={{ display: "none" }}>
-        Download Multi-line Text File (Blob)
-      </a>
+      <button className="save-file" onClick={saveBinFile}>
+        Save Bin. File
+      </button>
     </>
   );
 }
