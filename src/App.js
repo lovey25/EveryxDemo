cfbed3226974e91d88ea69246b8c57c554a4762d
kwonkyo <kwonkyo@gmail.com>
2023-02-27 06:38:13 +0000

    600
    전역변수를 사용하지 않고 속성으로 자식 컴포넌트에 데이터 전달하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
+const Type = (props) => {
+  const [state, setState] = useState("");
+
+  return (
+    <div>
+      선택:
+      <select onChange={(e) => setState(e.target.value)} value={state}>
+        <option value="type-1">이건 타입 1번</option>
+        <option value="type-2">이건 타입 2번</option>
+      </select>
+      <div>{props.children(state)}</div>
+    </div>
+  );
+};
+
+const Result = ({ type }) => {
+  return <div>{type}</div>;
+};
+
 export default function App() {
   return (
     <>
-      <div>everyX</div>
+      <Type>{(opt) => <Result type={opt} />}</Type>
     </>
   );
 }
